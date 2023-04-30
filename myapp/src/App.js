7a3d3f25965a016b5eb8e8f238e3791032e47c76
Radhasabu <2020bit063@sggs.ac.in>
2023-04-30 16:44:09 +0530

    initial commit

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+function App() {
+  return <Router>
+    <div>hii how are you</div>
+    <Routes>
+      <Route/>
+    </Routes>
+  </Router>
+}
+
+export default App;
